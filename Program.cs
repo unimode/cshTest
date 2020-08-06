6940649c4c5a9969c480f4db8b6471bc3abd7c08
Roman A. Paramonov <paramra@mail.ru>
2020-08-06 09:43:41 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace cshTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            String str = "Test 1";
+            Console.WriteLine(str);
+        }
+    }
+}
